d7b607fc4828224ba8b2d6b8d10278d7c2b87595
KarimCarl <cndungu109@gmail.com>
2022-11-28 17:09:41 +0300

    Changed the code format into a function format

--- speed-detector.js
+++ speed-detector.js
@@ -1,4 +1,4 @@
-// Speed Detector (Toy Problem)
+// Speed Detector
 // Write a program that takes as input the speed of a car e.g 80. 
 // If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), 
 // it should give the driver one demerit point and print the total number of demerit points.
@@ -7,35 +7,22 @@
 // If the driver gets more than 12 points, the function should print: “License suspended”.
 
 
-let carSpeed = prompt('speed of car');
-
-
-function speedDetector (carSpeed) {
-    for(let i = 70; i < 70; i += 2) {
-        console.log(i)
+function speedDetector() {
+    let speedLimit = 70;
+    let carSpeed = prompt();
+    
+    if (carSpeed <= speedLimit) {
+        return `Ok.`;
+    } else if(carSpeed > speedLimit) {
+        let demeritPoints = (carSpeed-speedLimit)/5;
+        if(demeritPoints<=12) {
+            return `${demeritPoints}`;
+        } else if (demeritPoints > 12){
+            return `License Suspended!!!!!`;
+        }
     }
 
 }
-speedDetector (carSpeed);
-
-
-
-
-
-let carSpeed = prompt('speed of car');
-
-let demeritPoint = (carSpeed += 5);
-
-console.log(carSpeed);
-
-
-
-
-
-
-
-
-
-
+speedDetector();
 
 
