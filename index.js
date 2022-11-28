9212eec6a09b3a9b4efcbc6f08c4b20f593b2312
KarimCarl <cndungu109@gmail.com>
2022-11-28 22:35:16 +0300

    Added more comments

--- index.js
+++ index.js
@@ -3,25 +3,28 @@
 // Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
 // A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 
-//studentMarks is the variable name of the marks provided by the students
-let studentMarks = prompt('Enter your marks');
 
 //Whichever condition the provided marks fit, the block inside the curly brackets is executed. 
-if (studentMarks >79) {
+
+function studentGrade () {
+   //studentMarks is the variable name of the marks provided by the students
+   let studentMarks = prompt('Enter your marks');
+
+    if (studentMarks >79) {
         //if student inputs marks greater than 79, it prints an A!
-        console.log(`A`);
+        return `A`;
     }   else if (studentMarks >= 60) {
         //if student inputs marks greater than or equal to 60 and less than 79, it prints a B!
-        console.log(`B`);
+        return `B`;
     }   else if (studentMarks >49) {
         //if student inputs marks greater than 49 and less than 60, it prints a C!
-        console.log(`C`);
+        return `C`;
     }   else if (studentMarks >=40){
         //marks greater than or equal to 40 and less than 50, it prints a D! 
-        console.log(`D`);
+        return `D`;
     }   else if (studentMarks <40) {
         //marks less than 40, prints an E!
-        console.log(`E`);
+        return `E`;
     }
 
 else {
@@ -29,7 +32,9 @@ else {
     console.log(`Please, enter valid student marks.`);
 }
 
-
+}
+//this calls the studentGrade function
+studentGrade();
 
 
 
