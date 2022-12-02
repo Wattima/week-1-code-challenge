9076fd09ad61e3c8716a013eae43ae67fa90c14a
KarimCarl <cndungu109@gmail.com>
2022-12-03 00:04:28 +0300

    Removed the else statement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Challenge 1: Student Grade Generator (Toy Problem)
 
-// Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
-// A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+// Write that prompts the user to input student marks. The input should be between 0 and 100. 
+//Then output the correct grade: A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 
 
 //Whichever condition the provided marks fit, the block inside the curly brackets is executed. 
@@ -10,7 +10,7 @@ function studentGrade () {
    //studentMarks is the variable name of the marks provided by the students
    let studentMarks = prompt('Enter your marks');
 
-    if (studentMarks >79) {
+    if (studentMarks > 79) {
         //if student inputs marks greater than 79, it prints an A!
         return `A`;
     }   else if (studentMarks >= 60) {
@@ -26,11 +26,6 @@ function studentGrade () {
         //marks less than 40, prints an E!
         return `E`;
     }
-
-else {
-    //if the student enters anything outside the set parameters, the code prints this
-    console.log(`Please, enter valid student marks.`);
-}
 
 }
 //this calls the studentGrade function
