37a9c0e3858906598831bc087222497cd7e5d0bd
KarimCarl <cndungu109@gmail.com>
2022-12-02 23:25:51 +0300

    Updated comments for the first part of the code

--- salary-calculator.js
+++ salary-calculator.js
@@ -7,10 +7,13 @@
   //if basic salary is equal or less than 24000,  paye is 10 percent
   if (basicSalary <= 24000) {
     paye = basicSalary / 10;
-  } else if (basicSalary >= 24001 && basicSalary <= 32333) {
-    //25% PAYE  for 24,001 - 32,333 monthly
+  } 
+  //if basic salary is greater than 24000 and less than or equal to 32333, paye is 25 percent
+  else if (basicSalary >= 24001 && basicSalary <= 32333) {
     paye = basicSalary / 4;
-  } else if ((basicSalary > 32, 333)) {
+  } 
+  //if basic salary is greater than 32333,  paye is 30 percent
+  else if ((basicSalary > 32, 333)) {
     //30% PAYE for above 32,333
     paye = (basicSalary * 3) / 10;
   }
